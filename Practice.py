a3ab976047a5755c005265a53088604d9f45659a
Hyeonki <sadpo5660@gmail.com>
2024-03-15 19:18:50 +0900

    python

---
+++ Practice.py
@@ -0,0 +1,29 @@
+print(5+1)
+print(5*1)
+print(5/1)
+print(1-5)
+print('풍선')
+print("dog")
+print("zzz"*3)
+print("zzzzzz")
+
+print(5 < 10)
+print(5 > 10)
+
+print(not 5 > 10)
+#print('not은 반대로 만든다')
+
+animal = "동물"
+name = "고양이"
+age = 4
+
+print("우리집 " + animal + "의 종류는 " + name + "입니다")
+print("그 " + name + "의 나이는 " + str(age) +"입니다")
+print("그 " , name , "의 나이는 " , age ,"입니다")
+# +대신 ,로 사용하면 형변환을 할필요가 없으나 ,위치에 띄어쓰기가 된다
+#print("인트형이나 bool형은 str()을통해 형변환을 해줘야한다")
+''' 이걸 통해 여러문장 
+주석처리를 한다'''
+# 1
+# 2
+# 3 ctrl+/를하면 일괄 주석처리
